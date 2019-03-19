6491faa40f6c066c9fb1dee0e03c272d298a6bde
sjh2428 <pok_gare@naver.com>
2019-03-19 21:52:25 +0900

    Updates

---
+++ precache-manifest.70044e245208935e3d583a81d4fafff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92d4fa86bc69bea1f7a5",
+    "url": "/movie_app/static/css/main.e532da6e.chunk.css"
+  },
+  {
+    "revision": "92d4fa86bc69bea1f7a5",
+    "url": "/movie_app/static/js/main.656e4097.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "df0b9ca0f81249544de6",
+    "url": "/movie_app/static/js/2.1267fdac.chunk.js"
+  },
+  {
+    "revision": "b84fff985826624cb32e1008e36ed890",
+    "url": "/movie_app/index.html"
+  }
+];
